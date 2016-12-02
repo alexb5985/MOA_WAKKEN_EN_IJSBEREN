265cff9ad4d88ee2bc42281ae122886b9e2a1e1c
Aliaselias <basgrave@hotmail.com>
2016-12-02 11:40:33 +0100

    Added score class

--- app/src/main/java/com/example/gebruiker/moa_wakken_en_ijsberen/Score.java
+++ app/src/main/java/com/example/gebruiker/moa_wakken_en_ijsberen/Score.java
@@ -5,6 +5,38 @@ package com.example.gebruiker.moa_wakken_en_ijsberen;
  */
 
 public class Score {
+    private String name;
+    private int goodGuesses;
+    private int wrongGuesses;
 
+    public int getWrongGuesses() {
+        return wrongGuesses;
+    }
+
+    public void setWrongGuesses(int wrongGuesses) {
+        this.wrongGuesses = wrongGuesses;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getGoodGuesses() {
+        return goodGuesses;
+    }
+
+    public void setGoodGuesses(int goodGuesses) {
+        this.goodGuesses = goodGuesses;
+    }
+
+    public Score(String name, int goodGuesses, int wrongGuesses) {
+        this.name = name;
+        this.goodGuesses = goodGuesses;
+        this.wrongGuesses = wrongGuesses;
+    }
 
 }
