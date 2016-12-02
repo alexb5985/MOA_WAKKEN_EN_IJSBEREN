6ffb268e302c92c005742ec69f9c863ccfc74134
laurens25 <platvis@live.nl>
2016-12-02 11:07:17 +0100

    Menufragment edited

--- app/src/main/java/com/example/gebruiker/moa_wakken_en_ijsberen/MainActivity.java
+++ app/src/main/java/com/example/gebruiker/moa_wakken_en_ijsberen/MainActivity.java
@@ -1,20 +1,24 @@
 package com.example.gebruiker.moa_wakken_en_ijsberen;
 
 import android.net.Uri;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity implements FragmentMenu.OnFragmentInteractionListener{
+public class MainActivity extends AppCompatActivity{
 
+    FragmentManager fm;
+    FragmentTransaction ft;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //TEST TEST
+        fm = getSupportFragmentManager();
+        ft = fm.beginTransaction();
+        ft.add(R.id.FrameFragment, new FragmentMenu());
+        ft.commit();
     }
 
-    @Override
-    public void onFragmentInteraction(Uri uri) {
-
-    }
 }
