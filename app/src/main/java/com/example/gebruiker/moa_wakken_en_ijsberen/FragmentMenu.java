9fc886b5a84c944768f9ce9f328185541af080d2
laurens25 <platvis@live.nl>
2016-12-02 15:31:20 +0100

    Fragment edit
    Gamefragment toegevoegt

--- app/src/main/java/com/example/gebruiker/moa_wakken_en_ijsberen/FragmentMenu.java
+++ app/src/main/java/com/example/gebruiker/moa_wakken_en_ijsberen/FragmentMenu.java
@@ -8,14 +8,23 @@ import android.view.ViewGroup;
 import android.widget.Button;
 
 public class FragmentMenu extends Fragment {
-    Button btnUser, btnSettings, btnHelp;
+    Button btnGame, btnUser, btnSettings, btnHelp;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_menu, container, false);
+        btnGame = (Button) v.findViewById(R.id.btnGame);
         btnUser = (Button) v.findViewById(R.id.btnUser);
         btnSettings = (Button) v.findViewById(R.id.btnSettings);
         btnHelp = (Button) v.findViewById(R.id.btnHelp);
 
+        btnGame.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                android.support.v4.app.FragmentTransaction ft = getActivity().getSupportFragmentManager().beginTransaction();
+                ft.replace(R.id.FrameMain, new FragmentGame());
+                ft.commit();
+            }
+        });
         btnUser.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
