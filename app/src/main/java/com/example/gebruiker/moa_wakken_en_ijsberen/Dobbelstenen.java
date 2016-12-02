73c665a73467e980fe95ad6c9ee12a2c7646dcc9
loekgosen <loek5@live.com>
2016-12-02 11:13:42 +0100

    dobbelsteen edit
    dobbelsteen edit

--- app/src/main/java/com/example/gebruiker/moa_wakken_en_ijsberen/Dobbelstenen.java
+++ app/src/main/java/com/example/gebruiker/moa_wakken_en_ijsberen/Dobbelstenen.java
@@ -1,8 +1,14 @@
 package com.example.gebruiker.moa_wakken_en_ijsberen;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by Loek on 2-12-2016.
  */
 
 public class Dobbelstenen {
-}
+   public ArrayList<Integer> stenen;
+    
+
+        }
