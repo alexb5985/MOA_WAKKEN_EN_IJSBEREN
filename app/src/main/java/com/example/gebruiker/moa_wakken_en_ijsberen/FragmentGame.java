fd21dc4da9fe4b2405e574bf30b17d04ee96b80d
laurens25 <platvis@live.nl>
2016-12-19 17:28:01 +0100

    Opslag gewijzigd
    Slaat nu altijd op  (goed en fout) tenzij er nix is ingevult

--- app/src/main/java/com/example/gebruiker/moa_wakken_en_ijsberen/FragmentGame.java
+++ app/src/main/java/com/example/gebruiker/moa_wakken_en_ijsberen/FragmentGame.java
@@ -173,12 +173,12 @@ public class FragmentGame extends Fragment {
         score.setName(preferences.getString("Name", ""));
 
         //Voegt score toe aan database, en voegt extra dobbelsteen toe als alles goed is
-        if (score.getWrongGuesses() == 0 && dices < 8) {
             score.setTimeTaken(seconds - secondsTemp);
             score.setNumberDice(dices);
-            dices = dices + 1;
             DBHandler handler = new DBHandler(getContext());
             handler.addScore(score);
+        if (score.getWrongGuesses() == 0 && dices < 8) {
+            dices = dices + 1;
         }
     }
 
